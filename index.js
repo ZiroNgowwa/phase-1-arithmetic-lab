1987ef908ab308d3710eeab7598af010606f32c3
Ngowwa-Ziro <joseph.ngowwa@student.moringaschool.com>
2022-08-23 11:19:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 100;
+let num4 = 25;
+
+const mod = num3 / num4;
+
+const max = Math.max(5, 10, 15, 20);
